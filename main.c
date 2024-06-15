e66ff53ab2ae5b4c0b742c358022aa6813d3f7d0
M0stafa077 <ma4190243@gmail.com>
2024-06-16 00:53:21 +0300

    Testing hash_table/quadratic probing

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
-#include "Hash-Table/Linear-Probing/hash_table.h"
+// #include "Hash-Table/Linear-Probing/hash_table.h"
+#include "Hash-Table/Quadratic-Probing/hashTable_Quadratic.h"
 
 #define HASH_TABLE_SIZE    10
 
 int main()
 {
-    Hash_Table_t* table = HashTable_Init(HASH_TABLE_SIZE);
-    HashTable_Insert(table, 10);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 20);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 30);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 40);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 50);
-    HashTable_PrintTable(table);
+    Hash_Table_t* table = HashTable_Quad_Init(HASH_TABLE_SIZE);
+    HashTable_Quad_Insert(table, 10);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 20);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 30);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 40);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 50);
+    HashTable_Quad_PrintTable(table);
 
-    HashTable_Insert(table, 60);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 70);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 80);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 90);
-    HashTable_PrintTable(table);
-    HashTable_Insert(table, 100);
-    HashTable_PrintTable(table);
-        HashTable_Insert(table, 110);
-    HashTable_PrintTable(table);
-    // HashTable_PrintTable(table);
-    // HashTable_Remove(table, 5);
-    // HashTable_Remove(table, 1000);
-    // HashTable_PrintTable(table);
+    HashTable_Quad_Insert(table, 60);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 70);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 80);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 90);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 100);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Insert(table, 110);
+    HashTable_Quad_PrintTable(table);
+    HashTable_Quad_Remove(table, 5);
+    HashTable_Quad_Remove(table, 1000);
+    HashTable_Quad_PrintTable(table);
     
-    // HashTable_Free(&table);
-    // HashTable_PrintTable(table);
+    HashTable_Quad_Free(&table);
+    HashTable_Quad_PrintTable(table);
 
     return 0;
 }
